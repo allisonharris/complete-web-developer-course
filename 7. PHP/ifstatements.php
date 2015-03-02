19c06106e6da364cd686da4dadbe6df4a8024203
Allison Harris <allison.harris23@gmail.com>
2015-03-02 17:36:03 -0600

    through lecture 174 (PHP if statements)

---
+++ 7. PHP/ifstatements.php
@@ -0,0 +1,20 @@
+<?php
+
+	$number = 1;
+
+	$otherNumber = 3;
+
+	$thirdNumber = 3;
+
+	if ($number == $otherNumber AND $number == $thirdNumber) {
+
+		echo "True!";
+	}
+
+	else {
+
+		echo "False!";
+	}
+
+
+?>
