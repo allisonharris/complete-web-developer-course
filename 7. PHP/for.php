b8697f7f34c18405a98f94513b64998a0d1c96c6
Allison Harris <allison.harris23@gmail.com>
2015-03-02 17:45:28 -0600

    through lecture 176 (PHP for and foreach)

---
+++ 7. PHP/for.php
@@ -0,0 +1,18 @@
+<?php
+
+	$array = array ("cat", "dog", "turtle", "kangaroo");
+
+	foreach ($array as $key => $value) {
+
+		echo "Key: $key Value: $value <br />";
+
+	}
+
+	/*
+	for ($i; $i <= 10; $i++) {
+
+		echo $i."<br />";
+	}
+	*/
+
+?>
