b275a9b9754acbe2f32089afa61a611719c1c178
Allison Harris <allison.harris23@gmail.com>
2015-03-17 09:26:10 -0500

    through lecture 192 (connecting to SQL database)

---
+++ 8. MySQL/connecting.php
@@ -0,0 +1,11 @@
+<?php
+
+	$link = mysqli_connect("localhost", "cl20-exampledb", "K7.7grb-Y", "cl20-exampledb");
+
+		if (mysqli_connect_error()) {
+
+			echo("Could not connect to database");
+
+	}
+	
+?>
